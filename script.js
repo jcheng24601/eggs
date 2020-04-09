3bd8c3887570a3750efefee472fcc8260c97abca
Jenny <jennyjqcheng@gmail.com>
2020-04-09 11:57:56 -0700

    Moved JS to separate file.

---
+++ script.js
@@ -0,0 +1,18 @@
+const eggs = document.querySelectorAll(".egg");
+var victory = document.querySelector(".victory");
+var counter = document.querySelector(".counter");
+var count = 0;
+function revealEgg() {
+	if (this.classList.contains("hidden")) {
+		this.classList.remove("hidden");
+		count += 1;
+		counter.innerHTML = count;
+	}
+	if (count == 3) {
+		victory.innerHTML = "Congrats! You've found all the eggs!";
+	}
+}
+function listenEgg(thing) {
+	thing.addEventListener("click", revealEgg);
+}
+eggs.forEach(listenEgg);
