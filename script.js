98cf7315ee5f2461c21c3a7da787dfac4c92e95f
Chris Cheng <chrisacheng@gmail.com>
2020-04-11 14:40:39 -0700

    Add countdown timer.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Variables.
 let egg_count = 0;
+let time_remaining = 300;
 
 // Selectors.
 const counter = document.querySelector(".counter");
+const timer = document.querySelector(".timer");
 
 function revealEgg() {
+    if (time_remaining <= 0) {
+        return;
+    }
     if (this.classList.contains("hidden")) {
         this.classList.remove("hidden");
         egg_count += 1;
@@ -16,9 +21,19 @@ function revealEgg() {
     }
 }
 
+function tick() {
+    if (time_remaining > 0) {
+        time_remaining -= 1;
+        timer.innerHTML = time_remaining;
+    }
+}
+
 function main() {
     const eggs = document.querySelectorAll(".egg")
     eggs.forEach(egg => egg.addEventListener("click", revealEgg));
+
+    timer.innerHTML = time_remaining;
+    setInterval(tick, 1000);
 }
 
 main()
