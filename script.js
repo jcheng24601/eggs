5b4d624f01957ed9b20a88ede88db183c3d61b81
Chris Cheng <chrisacheng@gmail.com>
2020-04-11 14:40:18 -0700

    Some code cleanup + add hint message.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const eggs = document.querySelectorAll(".egg");
-var victory = document.querySelector(".victory");
-var counter = document.querySelector(".counter");
-var count = 0;
+// Variables.
+let egg_count = 0;
+
+// Selectors.
+const counter = document.querySelector(".counter");
+
 function revealEgg() {
-	if (this.classList.contains("hidden")) {
-		this.classList.remove("hidden");
-		count += 1;
-		counter.innerHTML = count;
-	}
-	if (count == 132) {
-		victory.innerHTML = "Congrats! You've found all the eggs!";
-	}
+    if (this.classList.contains("hidden")) {
+        this.classList.remove("hidden");
+        egg_count += 1;
+        counter.innerHTML = egg_count;
+    }
+    if (egg_count == 132) {
+        let victory = document.querySelector(".victory");
+        victory.innerHTML = "Congrats! You've found all the eggs!";
+    }
 }
-function listenEgg(thing) {
-	thing.addEventListener("click", revealEgg);
+
+function main() {
+    const eggs = document.querySelectorAll(".egg")
+    eggs.forEach(egg => egg.addEventListener("click", revealEgg));
 }
-eggs.forEach(listenEgg);
+
+main()
