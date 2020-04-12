de0257e7d6c2b8d25bd83737a9f100138fe70aa0
Jenny Cheng <31137328+jcheng1203@users.noreply.github.com>
2020-04-11 19:38:50 -0700

    Adding results badges. (#10)

--- script.js
+++ script.js
@@ -6,19 +6,28 @@ let timer = null;
 // Selectors.
 const counter_selector = document.querySelector("#counter span");
 const timer_selector = document.querySelector("#timer span");
+const result = document.querySelector("#result-msg");
+const results_table = document.querySelector("#results")
+
+const magikarp = document.querySelector("#magikarp");
+const snorlax = document.querySelector("#snorlax");
+const slowbro = document.querySelector("#slowbro");
+const chansey = document.querySelector("#chansey");
+const dragonite = document.querySelector("#dragonite");
+const articuno = document.querySelector("#articuno");
 
 function revealEgg() {
     if (time_remaining <= 0) {
         return;
     }
+
     if (this.classList.contains("hidden")) {
         this.classList.remove("hidden");
         egg_count += 1;
         counter_selector.innerHTML = egg_count;
     }
+
     if (egg_count == 132) {
-        let victory = document.querySelector("#victory");
-        victory.innerHTML = "Congrats! You've found all the eggs!";
         clearInterval(timer)
     }
 }
@@ -28,8 +37,26 @@ function tick() {
         time_remaining -= 1;
         timer_selector.innerHTML = time_remaining;
     }
+
     if (time_remaining <= 30) {
         document.querySelector("#timer").classList.add("red");
+    }
+
+    if (time_remaining <= 0) { 
+        results_table.classList.remove("display-none");
+        if (egg_count <= 25) {
+            magikarp.classList.add("highlight");
+        } else if (egg_count <=50) {
+            snorlax.classList.add("highlight");
+        } else if (egg_count <=75) {
+            slowbro.classList.add("highlight");
+        } else if (egg_count <=100) {
+            chansey.classList.add("highlight");
+        } else if (egg_count <=131) {
+            dragonite.classList.add("highlight");
+        } else {
+            articuno.classList.add("highlight");
+        }
     }
 }
 
